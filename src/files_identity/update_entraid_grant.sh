50e4ca1c1c6c0950bc00889df2bd97ef9c1bf2ed
Yohei Iwasaki <12.yohei.iwasaki.04@gmail.com>
2026-07-12 16:30:25 +0900

    update command

--- src/files_identity/update_entraid_grant.sh
+++ src/files_identity/update_entraid_grant.sh
@@ -5,4 +5,4 @@ STORAGE_ACCOUNT="<Storage Account Name>"
 OID=$(az ad sp list --display-name "[Storage Account] ${STORAGE_ACCOUNT}.file.core.windows.net" --query "[0].id" -o tsv)
 APP_ID=$(az ad sp show --id ${OID} --query appId -o tsv)
 
-az ad app permission grant --id ${APP_ID} --api 00000003-0000-0000-c000-000000000000 --scope User.Read profile openid
+az ad app permission admin-consent --id ${APP_ID}
